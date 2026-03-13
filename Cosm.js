cb26def6172f6e162b7932f682c7ccf44e792f3f
Elgars <elgars.znotins@gmail.com>
2026-03-13 19:59:15 +0200

    Beidzot kaut kas derigs dudi, pieliku daudzas lapas, un interaktivos elementus, felin ;) proud!

---
+++ Cosm.js
@@ -0,0 +1,6 @@
+document.addEventListener("mousemove", e => {
+  let x = e.clientX / window.innerWidth;
+  let y = e.clientY / window.innerHeight;
+
+  document.body.style.backgroundPosition = (x*1000) + "px " + (y*1000) + "px";
+});
