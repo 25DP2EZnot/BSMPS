891da7f7a012970e27dcdd3fb876692e36968585
Elgars <elgars.znotins@gmail.com>
2026-03-26 19:33:51 +0200

    kautkaturos

--- Cosm.js
+++ Cosm.js
@@ -4,3 +4,25 @@ document.addEventListener("mousemove", e => {
 
   document.body.style.backgroundPosition = (x*1000) + "px " + (y*1000) + "px";
 });
+
+
+
+
+
+let count = 1;
+const links = [
+  document.getElementById('vid1'),
+  document.getElementById('vid2'),
+  document.getElementById('vid3'),
+  document.getElementById('vid4')
+];
+
+document.getElementById('clickImg').addEventListener('click', () => {
+  count++;
+  if(count <= 5){
+    videos[count-1].style.display = 'inline-block';
+  } else {
+    videos.forEach(v => v.style.display = 'none');
+    count = 1;
+  }
+});
