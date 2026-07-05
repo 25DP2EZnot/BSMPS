da8dfd4b34e7c88597ce8b009505bf94c4f2aba6
YourName <A251057EZ@rvt.lv>
2026-07-05 23:42:58 +0300

    pieliku itka iespeju runat ar putu, pieliku durvju puzli bilde, salaboju kameru, centos salabot televizoru bet nestrada... centsos rit

--- Cosm.js
+++ Cosm.js
@@ -1,13 +1,83 @@
 document.addEventListener("mousemove", e => {
   let x = e.clientX / window.innerWidth;
   let y = e.clientY / window.innerHeight;
-
   document.body.style.backgroundPosition = (x*1000) + "px " + (y*1000) + "px";
 });
 
 
 
 function toggleImage() {
-  var img = document.getElementById('topImage');
-  img.classList.toggle('show');
+  var imge = document.getElementById('topImage');
+  imge.classList.toggle('show');
 };
+
+
+
+const Door = document.getElementById("Door");
+const Box = document.getElementById("BOXERS_Shorts");
+const Camera = document.getElementById("Pojektors");
+
+
+Box.addEventListener('click', () => {
+  if (Box.style.top === '82px') {
+    Box.style.top = '137px';
+    Box.style.left = '-33px';
+    Door.style.pointerEvents = 'none';
+  } else {
+    Box.style.top = '82px';
+    Box.style.left = '517px';
+    Door.style.pointerEvents = 'auto';
+  }
+});
+
+Camera.addEventListener('click', () => {
+  if (Door.style.display === 'block') {
+    Door.style.display = 'none';
+  } else {
+    Door.style.display = 'block';
+  }
+});
+
+
+
+const gif1 = document.getElementById("gif1");
+const gif2 = document.getElementById("gif2");
+const gif3 = document.getElementById("gif3");
+const gif4 = document.getElementById("gif4");
+
+const vid1 = document.getElementById("vid1");
+const vid2 = document.getElementById("vid2");
+const vid3 = document.getElementById("vid3");
+const vid4 = document.getElementById("vid4");
+
+vid1.addEventListener('click', () => {
+  if (gif1.style.display === 'none') {
+    gif1.style.display = 'block';
+  } else {
+    gif1.style.display = 'none';
+  }
+});
+
+vid2.addEventListener('click', () => {
+  if (gif2.style.display === 'none') {
+    gif2.style.display = 'block';
+  } else {
+    gif2.style.display = 'none';
+  }
+});
+
+vid3.addEventListener('click', () => {
+  if (gif3.style.display === 'none') {
+    gif3.style.display = 'block';
+  } else {
+    gif3.style.display = 'none';
+  }
+});
+
+vid4.addEventListener('click', () => {
+  if (gif4.style.display === 'none') {
+    gif4.style.display = 'block';
+  } else {
+    gif4.style.display = 'none';
+  }
+});
