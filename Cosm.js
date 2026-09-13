a1fe359585244d21773c8e3fe71ddac041c98ba3
YourName <A251057EZ@rvt.lv>
2026-09-13 22:30:30 +0300

    Ja es visu izdarju pareizi varat pasveicint dulahnu Handiju dendiju grmatu plaukt bd, pie izejas no tas!

--- Cosm.js
+++ Cosm.js
@@ -112,3 +112,91 @@ vid4?.addEventListener('click', () => {
     gif4.style.display = 'block';
   }
 });
+
+
+
+let AtilknePressed = false;
+
+function changeImage(id, first, second,
+  firstTop, firstLeft, firstWidth, firstHeight,
+  secondTop, secondLeft, secondWidth, secondHeight) {
+
+    let img = document.getElementById(id);
+
+    if (img.src.includes(first)) {
+        img.src = second;
+        img.style.top = secondTop;
+        img.style.left = secondLeft;
+        img.style.width = secondWidth;
+        img.style.height = secondHeight;
+    } else {
+        img.src = first;
+        img.style.top = firstTop;
+        img.style.left = firstLeft;
+        img.style.width = firstWidth;
+        img.style.height = firstHeight;
+    }
+
+    if (id === "Atilkne" && !AtilknePressed) {
+        AtilknePressed = true;
+        HandyDandy();
+    }
+}
+
+function HandyDandy() {
+    LetOut = true;
+
+    let handy = document.getElementById("HandyDandy");
+    handy.style.display = "block";
+
+    setTimeout(() => {
+        let startTop = parseFloat(handy.style.top) || 491;
+        let startTime = performance.now();
+        let duration = 3000;
+        let distance = 2000;
+
+        function flyUp(time) {
+            let progress = (time - startTime) / duration;
+            if (progress > 1) progress = 1;
+            let ease = progress * progress;
+            handy.style.top = (startTop - distance * ease) + "px";
+
+            if (progress < 1) {
+                requestAnimationFrame(flyUp);
+            } else {
+                handy.style.left = "420px";
+                handy.style.top = "1000px";
+                handy.style.scale = "0.8";
+                handy.src = "IMG/Worm_houses/handdy_dandy.png";
+
+                let music2 = new Audio("AUD/Wormwood/Swush.mp3");
+                music2.play();
+
+                let startTime2 = performance.now();
+                let duration2 = 1000;
+
+                function moveToPlace(time2) {
+                    let progress2 = (time2 - startTime2) / duration2;
+
+                    if (progress2 > 1) progress2 = 1;
+
+                    let ease2 = 1 - Math.pow(1 - progress2, 3);
+
+                    handy.style.top = (1000 - 960 * ease2) + "px";
+
+                    if (progress2 < 1) {
+                        requestAnimationFrame(moveToPlace);
+                    } else {
+                        handy.style.top = "40px";
+
+                        alert("Sveiks! Emm... Izglābēj! Esmu Dulahāns Handy Dendijs, jā es māku mentāli runāt ar jums izmantojot kaut ko vārdā Alert! Man ir piedāvājums, kamēr es esmu šeit brīvībā, varu paturēt grāmatas, kas ir skapī, vienkārši noklikšķini uz kādu, un es to paņemšu rokā!");
+                    }
+                }
+
+                requestAnimationFrame(moveToPlace);
+            }
+        }
+
+        requestAnimationFrame(flyUp);
+    }, 500);
+}
